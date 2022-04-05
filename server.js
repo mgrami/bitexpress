57c0e2da0b4b5ec57cc32a09967890680589ae9e
mgrami <m.grami.m@gmail.com>
2022-04-05 09:27:29 -0700

    json middleware and status code 200

--- server.js
+++ server.js
@@ -29,8 +29,11 @@ import cors from 'cors'
 const app = express()
 
 app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
+  res.status(200)
 	res.json(allcoins)
 })
 
